0fd25bc209bfb8412d6ae3227476b2a2cceff955
Gaetan <gaetan.fomat@ynov.com>
2025-01-20 09:10:30 -0800

    button

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome!</Text>
       <StatusBar style="auto" />
-    </View>
+      <Button mode="contained" onPress={() => console.log('Pressed')}>test</Button>
+    </View>   
   );
 }
 
